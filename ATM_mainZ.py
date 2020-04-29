562b73def1b9bc3d3e20d8d5354ad271847a7169
juliotesla <flojulio@gmail.com>
2020-04-29 16:20:21 -0700

    lo del git

--- ATM_mainZ.py
+++ ATM_mainZ.py
@@ -11,6 +11,8 @@ import subprocess
 import RPi.GPIO as GPIO
 #============================================ ATM_main  abril 28  2020   =======
 # git config --global user.name 'juliotesla'
+# git config --global user.email'flojulio@gmail.com'
+# git config credential.helper  store
 
 class Window(QtGui.QWidget):
     def __init__(self):
